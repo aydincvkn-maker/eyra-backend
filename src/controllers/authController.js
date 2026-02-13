596f7b6757b39b7a9285c5daea31e67eeaf854cd
aydincvkn-maker <aydincvkn@gmail.com>
2026-02-13 22:55:03 +0300

    Auto-commit at 2026-02-13 22:55:03

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -485,12 +485,16 @@ exports.googleLoginWithToken = async (req, res) => {
     const token = createToken(user);
     const needsProfileSetup = !user.gender || user.gender === "other";
 
+    // Günlük giriş bonusu
+    const dailyBonus = await checkDailyLoginBonus(user);
+
     res.json({
       success: true,
       token,
       isNewUser,
       needsProfileSetup,
       user: buildUserPayload(user),
+      dailyBonus: dailyBonus.granted ? dailyBonus : undefined,
     });
   } catch (err) {
     console.error("Google token login error:", err);
