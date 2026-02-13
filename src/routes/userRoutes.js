b2e82c3e44b509407a9ab591b6db3266f1675b3d
aydincvkn-maker <aydincvkn@gmail.com>
2026-02-13 22:43:05 +0300

    Auto-commit at 2026-02-13 22:43:04

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const blockController = require("../controllers/blockController");
 const auth = require("../middleware/auth");
 const requirePermission = require("../middleware/requirePermission");
 const multer = require("multer");
