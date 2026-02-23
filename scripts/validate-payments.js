921e81c1b274fa15323937ba117e80da2ecf9bb0
aydincvkn-maker <aydincvkn@gmail.com>
2026-02-23 23:33:32 +0300

    auto: sync backend changes 2026-02-23 23:33:32

---
+++ scripts/validate-payments.js
@@ -0,0 +1,6 @@
+require("../src/services/paymentProviders/stripeProvider");
+require("../src/services/paymentService");
+require("../src/controllers/paymentController");
+require("../src/routes/paymentRoutes");
+
+console.log("payments-backend-ok");
