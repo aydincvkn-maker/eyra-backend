1a82ece84e6fe85478c1fa5f137b23fa51f3f8d4
aydincvkn-maker <aydincvkn@gmail.com>
2026-03-15 03:24:13 +0300

    auto: sync backend changes 2026-03-15 03:24:13

--- _test_routes.js
+++
@@ -1,9 +0,0 @@
-try {
-  require("./src/routes/chatRoutes");
-  require("./src/routes/userRoutes");
-  require("./src/routes/verificationRoutes");
-  console.log("ALL-OK");
-} catch (e) {
-  console.error("FAIL:", e.message);
-  process.exit(1);
-}
