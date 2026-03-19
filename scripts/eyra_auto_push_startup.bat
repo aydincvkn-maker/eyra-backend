a458391769a65f7cd27f75347840d25fb08edd5f
aydincvkn-maker <aydincvkn@gmail.com>
2026-03-20 00:45:51 +0300

    auto: 4 files | 2026-03-20 00:45:51

--- scripts/eyra_auto_push_startup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:: EYRA Auto Push - Bilgisayar acilinca otomatik baslar
-:: Sadece Backend icin auto-commit scriptini baslatir
-
-start "EYRA Backend Auto Push" powershell -WindowStyle Minimized -ExecutionPolicy Bypass -File "C:\Users\Casper\Desktop\eyra-backend\scripts\auto_commit_push.ps1"
